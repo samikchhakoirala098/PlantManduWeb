ebe66784beb4098f2ea34bff008ddec4db3f4f94
samikchhakoirala098 <Samikchhakoirala0@gmail.com>
2026-03-04 11:15:31 +0545

    Backend files tracked

---
+++ backend/database/db.js
@@ -0,0 +1,26 @@
+const { Sequelize } = require('sequelize');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+        logging: false,
+    }
+);
+
+const connectDB = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('PostgreSQL connected via Sequelize');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+};
+
+module.exports = { sequelize, connectDB };
